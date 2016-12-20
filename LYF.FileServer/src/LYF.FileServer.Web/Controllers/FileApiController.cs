e6f838aac2b325c24354480a92ba5eea214e7ed8
刘艳飞 <18811505262@163.com>
2016-12-20 18:05:07 +0800

    优化了request和response的header信息的下载

--- LYF.FileServer/src/LYF.FileServer.Web/Controllers/FileApiController.cs
+++ LYF.FileServer/src/LYF.FileServer.Web/Controllers/FileApiController.cs
@@ -10,7 +10,7 @@ using LYF.FileServer.Web.Services;
 using LYF.FileServer.Web.Model;
 using System.Net.Http;
 using Microsoft.AspNetCore.Http;
-
+using Microsoft.Extensions.Primitives;
 
 namespace LYF.FileServer.Web.Controllers
 {
@@ -21,12 +21,29 @@ namespace LYF.FileServer.Web.Controllers
         public ActionResult Get(string filename)
         {
             FileEntity fileEntity = GetFileInfo(filename);
+            string fileFullPath = System.IO.Path.Combine(@"C:\", fileEntity.file_path, filename);
             if (fileEntity == null)
                 return NotFound();
-            FileStream fileStream = System.IO.File.Open(System.IO.Path.Combine(@"C:\", fileEntity.file_path, filename), FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
-            FileStreamResult result = new FileStreamResult(fileStream,fileEntity.file_mimetype);
-            Response.ContentLength = fileEntity.file_length;
-            return result;
+            FileStream fileStream = System.IO.File.Open(fileFullPath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+            long startByteIndex = 0;
+            long endByteIndex = fileEntity.file_length-1;
+            var reqTypedHeader = Request.GetTypedHeaders();
+            if (reqTypedHeader.Range!=null && reqTypedHeader.Range.Ranges.Count>0)
+            {
+                //断点续传处理，多range情景没有处理
+                startByteIndex = reqTypedHeader.Range.Ranges.First().From ?? 0;
+                endByteIndex = reqTypedHeader.Range.Ranges.First().To ?? fileEntity.file_length - 1;
+                Response.StatusCode = StatusCodes.Status206PartialContent;
+                var contentRange = new ContentRangeHeaderValue(startByteIndex, endByteIndex);
+                Response.Headers[HeaderNames.ContentRange] = contentRange.ToString();
+            }
+            Response.ContentType = fileEntity.file_mimetype;
+            var contentDisposition = new ContentDispositionHeaderValue("attachment");
+            contentDisposition.SetHttpFileName(fileEntity.file_name);
+            Response.Headers[HeaderNames.ContentDisposition] = contentDisposition.ToString();
+            Response.ContentLength = fileStream.Length;
+            Response.Headers[HeaderNames.AcceptRanges] = "bytes";
+            return new FileStreamResult(fileStream,fileEntity.file_mimetype);
         }
 
         public FileEntity GetFileInfo(string filename)
@@ -40,8 +57,7 @@ namespace LYF.FileServer.Web.Controllers
                 file_ext = fi.Extension,
                 file_mimetype = "application/octet-stream",
                 file_path = "uploads",
-                file_length = fi.Length,
-                file_md5 = fi.MD5()
+                file_length = fi.Length
             };
         }
     }
